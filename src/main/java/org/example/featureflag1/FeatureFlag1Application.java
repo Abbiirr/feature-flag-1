63989c2a76a8a77a55772e26c26a4ce479e02c37
Abir Hossain <abirhossain9@iut-dhaka.edu>
2024-12-11 12:35:33 +0600

    INIT project

---
+++ src/main/java/org/example/featureflag1/FeatureFlag1Application.java
@@ -0,0 +1,13 @@
+package org.example.featureflag1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeatureFlag1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FeatureFlag1Application.class, args);
+    }
+
+}
